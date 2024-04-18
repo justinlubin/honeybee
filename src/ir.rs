3d9016059ff24618c12d7f72b6b87cb90b7e57ae
Justin Lubin <jlubi333@gmail.com>
2024-04-18 11:49:09 -0700

    Work on parser

---
+++ src/ir.rs
@@ -0,0 +1,71 @@
+#[derive(Debug, Clone)]
+pub enum ValueType {
+    Int,
+    Str,
+}
+
+#[derive(Debug, Clone)]
+pub enum Value {
+    Int(u32),
+    Str(String),
+}
+
+pub type FactName = String;
+
+#[derive(Debug, Clone)]
+pub enum FactKind {
+    Annotation,
+    Analysis,
+}
+
+#[derive(Debug, Clone)]
+pub struct FactType {
+    pub params: Vec<(String, ValueType)>,
+}
+
+#[derive(Debug, Clone)]
+pub struct FactSignature {
+    pub name: FactName,
+    pub kind: FactKind,
+    pub typ: FactType,
+}
+
+#[derive(Debug, Clone)]
+pub struct Fact {
+    pub name: FactName,
+    pub args: Vec<(String, Value)>,
+}
+
+#[derive(Debug, Clone)]
+pub enum PredicateAtom {
+    Select { selector: String, arg: String },
+}
+
+#[derive(Debug, Clone)]
+pub enum PredicateRelation {
+    Eq(PredicateAtom, PredicateAtom),
+    Lt(PredicateAtom, PredicateAtom),
+}
+
+pub type Predicate = Vec<PredicateRelation>;
+
+#[derive(Debug, Clone)]
+pub struct ComputationSignature {
+    pub name: String,
+    pub params: Vec<(String, FactName)>,
+    pub ret: FactName,
+    pub precondition: Predicate,
+}
+
+pub enum Signature {
+    Fact(FactSignature),
+    Computation(ComputationSignature),
+}
+
+pub type Library = Vec<Signature>;
+
+#[derive(Debug, Clone)]
+pub struct Program {
+    pub annotations: Vec<Fact>,
+    pub goal: Fact,
+}
