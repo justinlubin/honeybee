851c77045745987f053e2c29caacb5d68d3de35e
Justin Lubin <jlubi333@gmail.com>
2024-04-18 18:08:19 -0700

    Add derivation tree

---
+++ src/derivation.rs
@@ -0,0 +1,47 @@
+use crate::ir::*;
+
+type Path<'a> = Vec<&'a str>;
+
+enum Tree<'a> {
+    Axiom(&'a Fact),
+    Goal(&'a Fact),
+    Step {
+        label: &'a str,
+        antecedents: Vec<(&'a str, Tree<'a>)>,
+        consequent: &'a Fact,
+    },
+}
+
+impl<'a> Tree<'a> {
+    fn replace(&mut self, mut path: Path, subtree: Tree<'a>) {
+        match path.pop() {
+            Some(name) => match self {
+                Tree::Step { antecedents, .. } => antecedents
+                    .iter_mut()
+                    .find_map(|(n, t)| if (name == *n) { Some(t) } else { None })
+                    .unwrap()
+                    .replace(path, subtree),
+                _ => panic!(),
+            },
+            None => *self = subtree,
+        }
+    }
+
+    fn goals(&self) -> Vec<(Path, &Fact)> {
+        match self {
+            Tree::Axiom(_) => vec![],
+            Tree::Goal(fact) => vec![(vec![], fact)],
+            Tree::Step {
+                antecedents, label, ..
+            } => antecedents
+                .iter()
+                .flat_map(|(_, subtree)| {
+                    subtree.goals().into_iter().map(|(mut path, fact)| {
+                        path.push(label);
+                        (path, fact)
+                    })
+                })
+                .collect(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod derivation;
 mod egglog_adapter;
 mod ir;
 mod syntax;
