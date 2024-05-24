df88ea6fc78c8603bdcc89d0ae11fdadeef460ae
Justin Lubin <jlubi333@gmail.com>
2024-05-24 10:34:13 -0700

    Add basic lib.py

---
+++ protocols/lib.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import altair as alt
+
+@dataclass
+class Seq:
+    @dataclass
+    class M:
+        sample: str
+        at: int
+        data: str
+
+    @dataclass
+    class D:
+        pass
+
+    m: M
+    d: D
+
+@dataclass
+class GeneQuantification:
+    @dataclass
+    class M:
+        sample: str
+        at: int
+
+    @dataclass
+    class D:
+        table: pd.DataFrame
+
+    m: M
+    d: D
+
+@dataclass
+class LibrarySizeHistogram:
+    @dataclass
+    class M:
+        sample: str
+        at: int
+
+    @dataclass
+    class D:
+        pass
+
+    m: M
+    d: D
+
+def bowtie(s: Seq) -> GeneQuantification.D:
+    filepath = s.data
+    command_output = ... # filepath
+    return pd.DataFrame(command_output)
+
+def kallisto(s: Seq) -> GeneQuantification.D:
+    filepath = s.data + s.at
+    command_output = ... # filepath
+    return pd.DataFrame(columns=command_output)
+
+def library_size_histogram(gq: GeneQuantification) -> LibrarySizeHistogram.D:
+    alt.Chart(gq.table).interactive()
+    return LibrarySizeHistogram.D()
