058b1b00797eba6f7b9f5ae290663e497d141966
Justin Lubin <jlubi333@gmail.com>
2024-06-14 15:28:33 -0700

    Fix publishing and add favicon

--- publish.sh
+++ publish.sh
@@ -1,39 +1,28 @@
 #!/usr/bin/env bash
 
+shopt -s extglob
+
 FROM=gui
-TO=gh-pages-new
+TO=gh-pages
 
 git clone git@github.com:justinlubin/honeybee.git \
   --branch gh-pages \
   --single-branch \
-  gh-pages-old
-
-mkdir $TO
-mv gh-pages-old/.git $TO
-
-for file in \
-  CNAME \
-  index.html \
-  main.css \
-  main.js \
-  biology.hblib \
-  biology.py
-do
-  cp $FROM/$file $TO/$file
-done
-
-for dir in \
-  pkg
-do
-  cp -a $FROM/$dir $TO/$dir
-done
+  $TO
 
 cd $TO
 git checkout gh-pages
+rm -rf !(.git)
+
+cd ../$FROM
+
+cp -a !(biome.json|package.json|package-lock.json|node_modules) ../$TO
+
+cd ../$TO
+git add -A
 git add * -f
 git commit -m "Upload"
 git push
 
 cd ../
-rm -rf gh-pages-old
 rm -rf $TO
