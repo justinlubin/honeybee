65af525e9a34de2ed005da914f9a588c6c58e149
Justin Lubin <jlubi333@gmail.com>
2024-09-16 17:29:17 -0700

    More progress on PBN

--- backend/src/lang.rs
+++ backend/src/lang.rs
@@ -29,8 +29,21 @@ pub type FactLibrary = HashMap<FactName, FactSignature>;
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum FormulaAtom {
     Param { arg: String, subarg: String },
-    FreeVar(String),
     Value(Value),
+}
+
+impl FormulaAtom {
+    pub fn eval(
+        &self,
+        args: &HashMap<String, HashMap<String, Value>>,
+    ) -> Value {
+        match self {
+            FormulaAtom::Param { arg, subarg } => {
+                args.get(arg).unwrap().get(subarg).unwrap().clone()
+            }
+            FormulaAtom::Value(v) => v.clone(),
+        }
+    }
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
@@ -38,12 +51,35 @@ pub enum Formula {
     True,
     Eq(FormulaAtom, FormulaAtom),
     Lt(FormulaAtom, FormulaAtom),
-    Fact(FactName, Vector<FormulaAtom>),
-    Not(Box<Formula>),
+    Fact(FactName, HashMap<String, FormulaAtom>),
     And(Box<Formula>, Box<Formula>),
-    Or(Box<Formula>, Box<Formula>),
-    Forall(String, Box<Formula>),
-    Exists(String, Box<Formula>),
+}
+
+impl Formula {
+    pub fn sat(
+        &self,
+        args: &HashMap<String, HashMap<String, Value>>,
+        facts: &FactSet,
+    ) -> bool {
+        match self {
+            Formula::True => true,
+            Formula::Eq(a1, a2) => a1.eval(args) == a2.eval(args),
+            Formula::Lt(a1, a2) => match (a1.eval(args), a2.eval(args)) {
+                (Value::Int(i1), Value::Int(i2)) => i1 < i2,
+                _ => panic!(),
+            },
+            Formula::Fact(fact_name, fact_args) => facts.contains(&Fact {
+                name: fact_name.clone(),
+                args: fact_args
+                    .iter()
+                    .map(|(x, a)| (x.clone(), a.eval(args)))
+                    .collect(),
+            }),
+            Formula::And(phi1, phi2) => {
+                phi1.sat(args, facts) && phi2.sat(args, facts)
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
@@ -59,9 +95,9 @@ pub type FunctionLibrary = HashMap<FunctionName, FunctionSignature>;
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Library {
-    pub inputs: FactLibrary,
-    pub outputs: FactLibrary,
-    pub functions: FunctionLibrary,
+    pub input: FactLibrary,
+    pub output: FactLibrary,
+    pub function: FunctionLibrary,
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
@@ -93,6 +129,7 @@ pub fn infer(
     match e {
         pbn::Exp::Hole(_) => None,
         pbn::Exp::App(f, args) => {
+            let fun_sig = lib.function.get(&f).unwrap();
             todo!()
         }
     }
