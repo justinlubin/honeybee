038a1d9ade531d8f41e27c351e5b9c20f5e69e9b
Justin Lubin <jlubi333@gmail.com>
2025-01-11 11:41:12 -0800

    Work on refactor

--- backend/src/next/pbn.rs
+++ backend/src/next/pbn.rs
@@ -160,9 +160,15 @@ pub mod core {
         arity: Vec<super::FParam>,
     }
 
-    pub struct Type(TypeName, IndexMap<MParam, Value>);
+    pub struct Type {
+        name: TypeName,
+        args: IndexMap<MParam, Value>,
+    }
 
-    pub struct AtomicProposition(PropName, IndexMap<MParam, Value>);
+    pub struct AtomicProposition {
+        name: PropName,
+        args: IndexMap<MParam, Value>,
+    }
 
     impl super::Function for Function {
         fn arity(&self) -> Vec<super::FParam> {
@@ -184,35 +190,180 @@ pub mod core {
         And(Box<Formula>, Box<Formula>),
     }
 
-    pub struct FunctionType(IndexMap<super::FParam, Type>, Type, Formula);
+    pub struct FunctionSignature {
+        params: IndexMap<super::FParam, Type>,
+        ret: Type,
+        condition: Formula,
+    }
 
-    pub type FunctionLibrary = IndexMap<BaseFunction, FunctionType>;
+    pub type FunctionLibrary = IndexMap<BaseFunction, FunctionSignature>;
 
-    pub struct Problem(FunctionLibrary, Vec<AtomicProposition>, Type);
+    pub struct Problem {
+        library: FunctionLibrary,
+        props: Vec<AtomicProposition>,
+        goal: Type,
+    }
 }
 
 mod datalog {
+    use indexmap::IndexMap;
+    use indexmap::IndexSet;
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
     pub enum ValueType {
         Int,
         Str,
     }
 
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
     pub enum Value {
         Int(i64),
         Str(String),
-        Var(String),
+        Var { name: String, typ: ValueType },
     }
 
+    pub type Domain = IndexSet<Value>;
+
+    impl Value {
+        fn check_domain(&self, dom: &Domain) -> Result<(), String> {
+            if !dom.contains(self) {
+                return Err();
+            }
+        }
+
+        pub fn infer(&self, dom: &Domain) -> ValueType {
+            match self {
+                Value::Int(_) => ValueType::Int,
+                Value::Str(_) => ValueType::Str,
+                Value::Var { typ, .. } => typ.clone(),
+            }
+        }
+
+        pub fn is_ground(&self) -> bool {
+            match self {
+                Value::Int(_) => true,
+                Value::Str(_) => true,
+                Value::Var { .. } => false,
+            }
+        }
+
+        pub fn is_abstract(&self) -> bool {
+            match self {
+                Value::Int(_) => false,
+                Value::Str(_) => false,
+                Value::Var { .. } => true,
+            }
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
     pub struct Relation(String);
 
-    pub struct Declaration(Relation, Vec<ValueType>);
+    pub enum RelationKind {
+        EDB,
+        IDB,
+    }
 
-    pub struct Fact(Relation, Vec<Value>);
+    pub struct RelationSignature {
+        params: Vec<ValueType>,
+        kind: RelationKind,
+    }
+
+    pub type RelationLibrary = IndexMap<Relation, RelationSignature>;
+
+    pub struct Fact {
+        relation: Relation,
+        args: Vec<Value>,
+    }
+
+    impl Fact {
+        pub fn check(
+            &self,
+            lib: &RelationLibrary,
+            dom: &Domain,
+        ) -> Result<(), String> {
+            let sig = lib
+                .get(&self.relation)
+                .ok_or(format!("unknown relation {:?}", self.relation))?;
+
+            if self.args.len() != sig.params.len() {
+                return Err(format!(
+                    "got {} args, expected {}",
+                    self.args.len(),
+                    sig.params.len()
+                ));
+            }
+
+            for (got_v, expected_vt) in self.args.iter().zip(sig.params.iter())
+            {
+                let got_vt = got_v.infer();
+                if got_vt != *expected_vt {
+                    return Err(format!(
+                        "argument {:?} of relation {:?} is type {:?} but expected {:?}",
+                        got_v,
+                        self.relation,
+                        got_vt,
+                        expected_vt
+                    ));
+                }
+
+                if !dom.contains(got_v) {
+                    return Err(format!(
+                        "argument {:?} of relation {:?} not in domain",
+                        got_v, self.relation,
+                    ));
+                }
+            }
+
+            Ok(())
+        }
+
+        pub fn is_ground(&self) -> bool {
+            self.args.iter().all(|v| v.is_ground())
+        }
+
+        pub fn is_abstract(&self) -> bool {
+            self.args.iter().all(|v| v.is_abstract())
+        }
+    }
 
     pub enum Predicate {
         Fact(Fact),
         PrimEq(Value, Value),
         PrimLt(Value, Value),
+    }
+
+    impl Predicate {
+        fn check_equal_types(v1: &Value, v2: &Value) -> Result<(), String> {
+            let vt1 = v1.infer();
+            let vt2 = v2.infer();
+            if vt1 != vt2 {
+                return Err(
+                    format!(
+                        "value {:?} has different type ({:?}) than value {:?} ({:?})",
+                        v1, vt1, v2, vt2
+                    )
+                );
+            }
+            Ok(())
+        }
+
+        pub fn check(&self, lib: &RelationLibrary) -> Result<(), String> {
+            match self {
+                Predicate::Fact(f) => f.check(lib),
+                Predicate::PrimEq(v1, v2) => Self::check_equal_types(v1, v2),
+                Predicate::PrimLt(v1, v2) => {
+                    let vt1 = v1.infer();
+                    if vt1 != ValueType::Int {
+                        return Err(format!(
+                            "value {:?} has type {:?}, expected Int",
+                            v1, vt1,
+                        ));
+                    }
+                    Self::check_equal_types(v1, v2)
+                }
+            }
+        }
     }
 
     pub struct Rule {
@@ -221,19 +372,41 @@ mod datalog {
         body: Vec<Predicate>,
     }
 
+    impl Rule {
+        fn check(&self, lib: &RelationLibrary) -> Result<(), String> {
+            self.head.check(lib)?;
+
+            if !self.head.is_abstract() {
+                return Err(format!(
+                    "head of rule {} must be abstract",
+                    self.name
+                ));
+            }
+
+            for p in &self.body {
+                p.check(lib)?;
+            }
+
+            Ok(())
+        }
+    }
+
     pub struct Program {
         domain: Vec<Value>,
-        idb_decls: Vec<Declaration>,
-        edb_decls: Vec<Declaration>,
+        lib: RelationLibrary,
         rules: Vec<Rule>,
-        edbs: Vec<Fact>,
+        ground_facts: Vec<Fact>,
+    }
+
+    impl Program {
+        fn check(&self) -> Result<(), String> {}
     }
 
     pub trait Engine {
         fn query(
             &self,
             program: Program,
-            query_declaration: Declaration,
+            query_signature: RelationSignature,
             query_rule: Rule,
         ) -> Vec<Vec<Value>>;
     }
