4c159b38a26a5a4d52e5cf6d57304807637fa011
Justin Lubin <jlubi333@gmail.com>
2025-03-13 10:54:18 -0700

    Add eval scripts

---
+++ RUN_EVAL.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+################################################################################
+## Set up variables and directories
+
+MODE="full"
+
+timestamp=$(date +'y%Y-m%m-d%d_hr%H-min%M-sec%S-ms%3N')
+
+mkdir -p "benchmark/data/$MODE/$timestamp"
+mkdir -p "benchmark/analysis/output/$MODE/$timestamp"
+
+backend_dir=$(realpath "backend")
+analysis_dir=$(realpath "benchmark/analysis")
+
+data_dir=$(realpath "benchmark/data/$MODE/$timestamp")
+output_dir=$(realpath "benchmark/analysis/output/$MODE/$timestamp")
+
+################################################################################
+## Benchmark synthesizers
+
+cd $backend_dir
+
+cargo build
+hb="./target/debug/honeybee"
+
+$hb benchmark \
+    --suite ../benchmark/suites/fin,../benchmark/suites/scal \
+    --algorithms PBNHoneybee,PBNHoneybeeNoMemo,NaiveEnumeration,PrunedEnumeration \
+    --replicates 5 \
+    --timeout 120 \
+    > "$data_dir/finscal.tsv"
+
+$hb benchmark \
+    --suite ../benchmark/suites/inf \
+    --algorithms PBNHoneybee,PBNHoneybeeNoMemo \
+    --replicates 5 \
+    --timeout 120 \
+    > "$data_dir/inf-pbn.tsv"
+
+$hb benchmark \
+    --suite ../benchmark/suites/inf \
+    --algorithms NaiveEnumeration,PrunedEnumeration \
+    --replicates 5 \
+    --timeout 120 \
+    --limit 0 \
+    > "$data_dir/inf-baseline.tsv"
+
+################################################################################
+## Combine results
+
+cd $data_dir
+
+cat \
+    finscal.tsv \
+    < (tail -n +2 inf-pbn.tsv) \
+    < (tail -n +2 inf-baseline.txt) \
+    > combined.tsv
+
+################################################################################
+## Generate graphs
+
+cd $analysis_dir
+
+uv run analyze.py 120 "$data_dir/combined.tsv" $output_dir
