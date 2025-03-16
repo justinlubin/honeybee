37dadbf8ad5b2e30265ce5b90ca80b9e01d27916
Justin Lubin <jlubi333@gmail.com>
2025-03-15 17:26:46 -0700

    Update kick-the-tires script

--- KICK_TIRES.sh
+++ KICK_TIRES.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Checking that uv is installed... (you should see a version string)"
+cd backend
+echo "*** Building Honeybee... (you should see no errors)"
+echo
 
-uv --version
+cargo build
 
 echo
-echo "Checking that Honeybee can be built and run... (you should see a version string)"
+echo "*** Checking that Honeybee can be built and run... (you should see a version string)"
+echo
 
-cd backend
 cargo run -- --version
+
+echo
+echo "*** Checking that uv is installed... (you should see a version string)"
+echo
+
+uv --version
