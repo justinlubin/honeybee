865971b593270896608c5e91e362f8006bae3212
Justin Lubin <jlubi333@gmail.com>
2025-05-12 15:11:18 -0700

    Add publish script

---
+++ frontend/publish.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+shopt -s extglob
+
+TO="__gh-pages"
+
+git clone git@github.com:justinlubin/honeybee.git \
+  --branch gh-pages \
+  --single-branch \
+  "$TO"
+
+cd "$TO"
+git checkout gh-pages
+rm -rf !(.git)
+
+cd ../
+
+cp -r www/. $TO
+
+cd $TO
+git add * -f
+git commit -m "Upload"
+git push
+
+cd ../
+rm -rf $TO
