9c40e9e7d91bda207ab257fa2ecfedf05c124767
Justin Lubin <jlubi333@gmail.com>
2025-10-28 10:24:16 -0700

    Upload

--- elm.js
+++ elm.js
@@ -6800,7 +6800,7 @@ var $elm$html$Html$Attributes$classList = function (classes) {
 };
 var $elm$html$Html$div = _VirtualDom_node('div');
 var $author$project$Version$shortVersion = '0.4.0';
-var $author$project$Version$fullVersion = $author$project$Version$shortVersion + '+5305d14';
+var $author$project$Version$fullVersion = $author$project$Version$shortVersion + '+bbcddfb';
 var $elm$html$Html$Attributes$href = function (url) {
 	return A2(
 		$elm$html$Html$Attributes$stringProperty,
