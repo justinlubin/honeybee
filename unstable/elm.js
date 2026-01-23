a2fe235449a9fde764a7f507ea479c18eeb45e4a
Justin Lubin <jlubi333@gmail.com>
2026-01-22 21:29:52 -0800

    Upload

--- unstable/elm.js
+++ unstable/elm.js
@@ -6788,7 +6788,7 @@ var $elm$html$Html$Attributes$classList = function (classes) {
 };
 var $elm$html$Html$div = _VirtualDom_node('div');
 var $author$project$Version$shortVersion = '0.4.0';
-var $author$project$Version$fullVersion = $author$project$Version$shortVersion + '+a65e8b3';
+var $author$project$Version$fullVersion = $author$project$Version$shortVersion + '+10cf211';
 var $elm$html$Html$Attributes$href = function (url) {
 	return A2(
 		$elm$html$Html$Attributes$stringProperty,
